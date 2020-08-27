53f6781ba62596cd3f0b9a0cd7d266fea988603f
Lisa Walters <campi_l@fortlewis.edu>
2020-08-27 01:39:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,30 @@
-# Add  code here!
+# A
+def prime?(num)
+
+  if 
+  num <= 1
+  puts "false"
+
+  elsif
+
+    num == 2
+    puts "true"
+
+  else
+
+i = 2
+
+arr = []
+while i < num
+answer = num % i
+arr.push(answer)
+i += 1
+end
+
+result = arr.any? {|number| number == 0}
+puts !result
+
+end 
+end
+
+prime?(8)dd  code here!
